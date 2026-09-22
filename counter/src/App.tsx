3d3b39e85fbe51e9df8ff5e436b0513e94191653
Divyansh <divyaansha2004@gmail.com>
2026-09-22 23:59:16 +0530

    feat: build basic counter component

---
+++ counter/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState , useEffect} from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+  //const increase = () => setCount((currentCount) => currentCount + 1)
+  const decrease = () =>
+    setCount((currentCount) => (currentCount > 0 ? currentCount - 1 : 0));
+  const reset = () => setCount(0);
+
+  return (
+    <div>
+      <div>
+        <h1>Counter</h1>
+        <p>{count}</p>
+        <div>
+          <button type="button" onClick={decrease}>
+            -
+          </button>
+          <button type="button" onClick={reset}>
+            Reset
+          </button>
+          <button type="button" onClick={() => setCount(count + 1)}>
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
