e0389970760728e0d43b65513ddd10da72362558
Divyansh <divyaansha2004@gmail.com>
2026-09-24 16:44:10 +0530

    feat: build employee directory interface

---
+++ employee-directory/src/App.tsx
@@ -0,0 +1,19 @@
+import EmployeeList from './components/EmployeeList'
+ 
+const App = () => {
+  return (
+    <>
+    <header>
+      <div>
+        
+      </div>
+      <div>
+
+      </div>
+    </header>
+    <EmployeeList/>
+    </>
+  )
+}
+ 
+export default App
